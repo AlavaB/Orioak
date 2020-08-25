4ac1b5df68abb2e7c8743d3cd39f4123bf66ebbf
Emilie Bojkovic <emilie.bojkovic@outlook.com>
2020-08-25 18:44:34 +0200

    chore (product page) : Elements implementation finalization all API elements have a dynamic loading

--- js/produit.js
+++ js/produit.js
@@ -3,79 +3,39 @@ let url = new URL(pageUrl);
 let id = url.searchParams.get("id");
 let apiUrl = "http://localhost:3000/api/furniture/" + id;
 
-let createSecondCol = document.createElement("div");
-let deliveryMessage = "Suivi de livraison en ligne ou sur demande";
-let refundMessage = "Satisfait ou remboursé (selon conditions générales)";
-let deadlinesMessage = "Respect des délais annonçés";
-
-function orderInfo(message) {
-    let info = document.createElement("div");
-    createSecondCol.appendChild(info);
-    let checkedImage = document.createElement("img");
-    checkedImage.src = "../images/checked.png";
-    checkedImage.classList.add("product-page-info");
-    checkedImage.setAttribute("alt", "checked button");
-    checkedImage.innerHTML = message;
-    info.appendChild(checkedImage);
-};
 
 function item(response) {
 
     let imageCol = document.getElementById("image-col");
     let infoCol = document.getElementById("info-col");
     let line = document.getElementById("line");
+    let selectVarnish = document.getElementById("varnish");
 
-    let createImage = document.createElement("img");
-    createImage.classList.add("picture");
+    let createImage = document.getElementById("product-image");
     createImage.style.backgroundImage = "url(" + response.imageUrl + ")";
-    imageCol.appendChild(createImage);
 
-    let createTitle = document.createElement("h1");
-    createTitle.classList.add("display-4", "title-product");
+    let createTitle = document.getElementById("title-product");
     createTitle.textContent = response.name;
-    infoCol.appendChild(createTitle);
 
-    let createLine = document.createElement("div");
-    createLine.classList.add("layout-line");
-    infoCol.appendChild(createLine);
-
-    let createPrice = document.createElement("h2");
-    createPrice.classList.add("display-4", "mt-3", "price");
+    let createPrice = document.getElementById("price-product");
     createPrice.textContent = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(response.price / 100);
-    infoCol.appendChild(createPrice);
 
-    let createVarnishSelector = document.createElement("label");
-    createVarnishSelector.setAttribute("for", "varnish");
-    createVarnishSelector.textContent = "Choisissez le vernis :"
-    infoCol.appendChild(createVarnishSelector);
-
-    let selectVarnish = document.createElement("select");
-    selectVarnish.setAttribute("name", "varnish");
-    selectVarnish.setAttribute("id", "varnish");
-    createVarnishSelector.appendChild(selectVarnish);
+    let descriptionCol = document.getElementById("description");
+    descriptionCol.textContent = response.description;
     
     /*let createVarnishOption = document.createElement("option");
     createVarnishOption.setAttribute("value", response.varnish.length);
     createVarnishOption.textContent = response.varnish.length;
     selectVarnish.appendChild(createVarnishOption);*/
-    
-    let createQuantitySelector = document.createElement("label");
-    createQuantitySelector.setAttribute("for", "quantity");
-    createQuantitySelector.textContent = "Indiquez la quantité souhaitée :";
-    infoCol.appendChild(createQuantitySelector);
 
-    let createQuantityOption = document.createElement("div");
-    createQuantityOption.classList.add("mb-lg-2");
-    createQuantitySelector.appendChild(createQuantityOption);
-    
-    let createQuantityInput = document.createElement("input");
-    createQuantityInput.setAttribute("type", "number");
-    createQuantityInput.setAttribute("name", "quantity");
-    createQuantityInput.setAttribute("id", "quantity");
-    createQuantityInput.setAttribute("value", 1);
-    createQuantityInput.setAttribute("min", 1);
-    createQuantityInput.setAttribute("max", 9);
-    createQuantityOption.appendChild(createQuantityInput);
+    for (let index = 0; index < response.varnish.length; index++) {
+        const element = response.varnish[index];
+        let createOptions = document.createElement("option");
+        createOptions.textContent = element;
+        selectVarnish.appendChild(createOptions);
+    }
+
+
 };
     
 
