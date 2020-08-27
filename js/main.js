33adab7a91f8fdc2e8cec79b004baad9fde90680
Emilie Bojkovic <emilie.bojkovic@outlook.com>
2020-08-27 14:19:17 +0200

    chore (confirmation page) : Layout improvment shortening of the line between products, responsive improvment

--- js/main.js
+++ js/main.js
@@ -1,17 +1,2 @@
-/*récupérer les données de l'API*/
-let imageResult = document.getElementsByClassName("picture1");
-
-function imageDataResult() {
-    imageDataResult = new XMLHttpRequest;
-    imageDataResult.open("GET", "http://localhost:3000/api/furniture");
-    imageDataResult.send();
-    imageDataResult.onreadystatechange = function() {
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            let response = JSON.parse(this.responseText);
-            imageResult.textContent = response.imageUrl;
-        }
-    }
-};
-
 
 
